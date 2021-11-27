96b5f5fb76af8c9fa9dd3b5af49e4fa43fe68337
Luan <santos.pereira.luan@gmail.com>
2021-11-26 23:16:17 -0300

    Limite da camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject target;
+    public Renderer mapRenderer;
+
+    private float rightLimit;
+    private float leftLimit;
+    private float topLimit;
+    private float bottomLimit;
+
+    void Start()
+    {
+        leftLimit = (mapRenderer.transform.position.x - mapRenderer.bounds.size.x / 2) + (Camera.main.orthographicSize * Camera.main.aspect);
+        rightLimit = (mapRenderer.transform.position.x + mapRenderer.bounds.size.x / 2) - (Camera.main.orthographicSize * Camera.main.aspect);
+        topLimit = (mapRenderer.transform.position.y + mapRenderer.bounds.size.y / 2) - Camera.main.orthographicSize;
+        bottomLimit = (mapRenderer.transform.position.y - mapRenderer.bounds.size.y / 2) + Camera.main.orthographicSize;
+    }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10), 5f * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z), 5f * Time.deltaTime);
+
+        float limitX = Mathf.Clamp(transform.position.x, leftLimit, rightLimit);
+        float limitY = Mathf.Clamp(transform.position.y, bottomLimit, topLimit);
+
+        transform.position = new Vector3(limitX, limitY, transform.position.z);
     }
 }
